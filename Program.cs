151fc43f0790a23c4b79d40f1ce0aa14c4b580bf
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-05-11 11:50:17 -0400

    Use DI

--- Program.cs
+++ Program.cs
@@ -1,30 +1,52 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
-using IHost host = Host.CreateDefaultBuilder(args).Build();
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<EmailOptions>(context.Configuration.GetSection("Email"));
+        services.AddTransient<EmailSender>();
+    })
+    .Build();
+
 var config = host.Services.GetRequiredService<IConfiguration>();
 
 var apiKey = config["SendGrid:ApiKey"];
-
-var emailOptions = config.GetSection("Email").Get<EmailOptions>();
-
 var client = new SendGridClient(apiKey);
-var message = new SendGridMessage
-{
-    From = emailOptions.From,
-    Subject = emailOptions.Subject,
-    PlainTextContent = emailOptions.Body
-};
-message.AddTo(emailOptions.To);
-var response = await client.SendEmailAsync(message);
 
-// A success status code means SendGrid received the email request and will process it.
-// Errors can still occur when SendGrid tries to send the email. 
-// If email is not received, use this URL to debug: https://app.sendgrid.com/email_activity 
-Console.WriteLine(response.IsSuccessStatusCode ? "Email queued successfully!" : "Something went wrong!");
+var emailSender = host.Services.GetRequiredService<EmailSender>();
+await emailSender.SendEmail(client);
+
+public class EmailSender
+{
+    private readonly EmailOptions emailOptions;
+
+    public EmailSender(IOptions<EmailOptions> emailOptions)
+    {
+        this.emailOptions = emailOptions.Value;
+    }
+
+    public async Task SendEmail(ISendGridClient client)
+    {
+        var message = new SendGridMessage
+        {
+            From = emailOptions.From,
+            Subject = emailOptions.Subject,
+            PlainTextContent = emailOptions.Body
+        };
+        message.AddTo(emailOptions.To);
+        var response = await client.SendEmailAsync(message);
+
+        // A success status code means SendGrid received the email request and will process it.
+        // Errors can still occur when SendGrid tries to send the email. 
+        // If email is not received, use this URL to debug: https://app.sendgrid.com/email_activity 
+        Console.WriteLine(response.IsSuccessStatusCode ? "Email queued successfully!" : "Something went wrong!");
+    }
+}
 
 public class EmailOptions
 {
