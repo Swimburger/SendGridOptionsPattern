48b201291f217218859263669decb8b407a5ad39
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-05-11 11:39:25 -0400

    Update config sections

--- Program.cs
+++ Program.cs
@@ -4,26 +4,26 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 
 IConfiguration config = new ConfigurationBuilder()
-	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-	.AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true, reloadOnChange: false)
-	.AddEnvironmentVariables()
-	.AddCommandLine(args)
-	.Build();
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true, reloadOnChange: false)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
 
 var apiKey = config["SendGrid:ApiKey"];
-var fromEmailAddress = config["Email:FromEmailAddress"];
-var fromName = config["Email:FromName"];
-var toEmailAddress = config["Email:ToEmailAddress"];
-var toName = config["Email:ToName"];
+var fromEmailAddress = config["Email:From:Email"];
+var fromName = config["Email:From:Name"];
+var toEmailAddress = config["Email:To:Email"];
+var toName = config["Email:To:Name"];
 var emailSubject = config["Email:Subject"];
 var emailBody = config["Email:Body"];
 
 var client = new SendGridClient(apiKey);
 var message = new SendGridMessage
 {
-	From = new EmailAddress(fromEmailAddress, fromName),
-	Subject = emailSubject,
-	PlainTextContent = emailBody
+    From = new EmailAddress(fromEmailAddress, fromName),
+    Subject = emailSubject,
+    PlainTextContent = emailBody
 };
 message.AddTo(new EmailAddress(toEmailAddress, toName));
 var response = await client.SendEmailAsync(message);
